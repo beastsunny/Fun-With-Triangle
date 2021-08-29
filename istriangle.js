df7d62185e34f99c6fe1a26aca85abe1b9879646
beastsunny <beastsunny06@gmail.com>
2021-08-29 15:55:33 +0530

    feat : function+clickhandler

--- istriangle.js
+++ istriangle.js
@@ -6,3 +6,15 @@ function calculateSumOfAngles(angle1, angle2, angle3) {
     const sumOfAngles = angle1 + angle2 + angle3;
     return sumOfAngles;
 }
+
+function isTriangle() {
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+    if (sumOfAngles === 180) {
+        outputE1.innerText = "Yay! The angles form a triangle";
+    } else {
+        outputE1.innerText = "Oops! The angles do not form a triangle";
+    }
+
+}
+
+isTriangleButton.addEventListener("click", isTriangle);
